df04fcc016ba6027285f6db49a5b74728c6dd59c
komcal <kom.sujautra@gmail.com>
2016-10-07 13:51:29 +0700

    add pacman image

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -1,16 +1,23 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameScreen extends ScreenAdapter {
+	private Texture pacmanImg;
 	private PacmanGame pacmanGame;
 	 
     public GameScreen(PacmanGame pacmanGame) {
         this.pacmanGame = pacmanGame;
+        pacmanImg = new Texture("pacman.png");
     }
     
     @Override
     public void render(float delta) {
-        System.out.println("Hello " + delta);
+        SpriteBatch batch = pacmanGame.batch;
+        batch.begin();
+        batch.draw(pacmanImg, 100, 100);
+        batch.end();
     }
 }

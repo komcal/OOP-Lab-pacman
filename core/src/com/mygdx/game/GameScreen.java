afdfc6826b9526062a457d82e936d232b7ff5437
komcal <kom.sujautra@gmail.com>
2016-10-07 14:01:44 +0700

    move pacman

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -1,6 +1,7 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
@@ -20,7 +21,12 @@ public class GameScreen extends ScreenAdapter {
     }
     
     private void update(float delta) {
-        x += 5;    
+    	if(Gdx.input.isKeyPressed(Keys.LEFT)) {
+            x -= 10;
+        }
+        if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
+            x += 10;
+        }  
     }
     
     @Override

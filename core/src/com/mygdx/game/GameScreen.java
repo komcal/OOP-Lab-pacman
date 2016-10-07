fe4a9aa6b06a460be8c972659944d7fea11f7689
komcal <kom.sujautra@gmail.com>
2016-10-07 14:35:18 +0700

    create Pacman class

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -6,26 +6,31 @@ import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 public class GameScreen extends ScreenAdapter {
 	private Texture pacmanImg;
 	private PacmanGame pacmanGame;
-    private int x;
-    private int y;
+	private Pacman pacman;
 	 
     public GameScreen(PacmanGame pacmanGame) {
         this.pacmanGame = pacmanGame;
         pacmanImg = new Texture("pacman.png");
-        x = 100;
-        y = 100;
+        pacman = new Pacman(100,100);
     }
     
     private void update(float delta) {
     	if(Gdx.input.isKeyPressed(Keys.LEFT)) {
-            x -= 10;
+    		pacman.move(Pacman.DIRECTION_LEFT);
         }
         if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
-            x += 10;
+        	pacman.move(Pacman.DIRECTION_RIGHT);
+        }
+        if(Gdx.input.isKeyPressed(Keys.UP)) {
+        	pacman.move(Pacman.DIRECTION_UP);
+        }
+        if(Gdx.input.isKeyPressed(Keys.DOWN)) {
+        	pacman.move(Pacman.DIRECTION_DOWN);
         }  
     }
     
@@ -36,7 +41,8 @@ public class GameScreen extends ScreenAdapter {
     	update(delta);
         SpriteBatch batch = pacmanGame.batch;
         batch.begin();
-        batch.draw(pacmanImg, x, y);
+        Vector2 pos = pacman.getPosition();
+        batch.draw(pacmanImg, pos.x, pos.y);
         batch.end();
     }
 }
